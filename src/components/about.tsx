0502c27f022a6bc85cf257fe7e5ec54dcde90cbb
BryanVanWinnendael <bryan.vanwinnendael@skynet.be>
2024-08-08 14:25:02 +0200

    Optimization

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -21,7 +21,7 @@ const About = () => {
   return (
     <div id="about" className="text-secondary font-medium h-screen">
       <article className="w-full h-full flex flex-col justify-center items-start p-7 sm:max-w-[90%]">
-        <p className="lg:text-7xl md:text-5xl text-4xl tracking-tight font-normal">
+        <p className="lg:text-7xl md:text-5xl text-3xl tracking-tight font-normal">
           My name is Bryan Van Winnendael. I&apos;m a {calculateAge()} year old
           full stack developer based in Belgium. My work is driven by a desire
           to create impactful solutions that address real-world problems. I
